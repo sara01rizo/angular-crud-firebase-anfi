bbae1e9b708ca541cc45d486f4978caf2a4cfad4
sara pulido <sara01rizo@gmail.com>
2023-06-16 01:38:18 +0200

    Create: data service for anfi

--- src/app/services/anfi.service.ts
+++ src/app/services/anfi.service.ts
@@ -1,9 +1,35 @@
 import { Injectable } from '@angular/core';
+import {AngularFireDatabase, AngularFireList } from '@angular/fire/compat/database';
+import { Task } from '../models/task.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AnfiService {
+  private dbPath = '/tasks';
 
-  constructor() { }
+  tasksRef: AngularFireList<Task>;
+
+  constructor(private db: AngularFireDatabase) {
+     this.tasksRef = db.list(this.dbPath);
+   }
+   getAll(): AngularFireList<Task> {
+    return this.tasksRef;
+   }
+
+   create(task: Task): any {
+    return this.tasksRef.push(task);
+   }
+
+   update(key: string, value: any): Promise<void> {
+    return this.tasksRef.update(key, value);
+  }
+
+  delete(key: string): Promise<void> {
+    return this.tasksRef.remove(key);
+  }
+
+  deleteAll(): Promise<void> {
+    return this.tasksRef.remove();
+  }
 }
