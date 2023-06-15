6eae24441d10b64320428b06d2e9a026c91c7fda
sara pulido <sara01rizo@gmail.com>
2023-06-16 01:56:45 +0200

    Component: for creatin Object to add a task

--- src/app/components/add-task/add-task.component.ts
+++ src/app/components/add-task/add-task.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, Injectable } from '@angular/core';
+import { Task } from 'src/app/models/task.model';
+import { TaskService } from 'src/app/services/anfi.service';
 
 @Component({
   selector: 'app-add-task',
   templateUrl: './add-task.component.html',
   styleUrls: ['./add-task.component.css']
 })
+
+@Injectable({
+  providedIn: 'root'
+})
+
 export class AddTaskComponent {
+  task: Task = new Task();
+  submitted = false;
+
+  constructor(private taskService: TaskService) {}
+
+  saveTask(): void {
+    this.taskService.create(this.task).then(() => {
+      console.log('Create new task successfully!');
+      this.submitted = true;
+    });
+  }
+
+  newTask(): void {
+    this.submitted =false;
+    this.task = new Task();
+  }
 
 }
