0b3aa3a928a7fbae2330ca336a9d627c2810fcae
sara pulido <sara01rizo@gmail.com>
2023-06-16 00:40:52 +0200

    Create: form from angular anfi, appComponents comp

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FireStore, collection, addDoc } from '@angular/fire/firestore';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,17 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-crud-firebase-anfi';
+
+  constructor(private firestore: Firestore) {}
+
+  addData(f: any) {
+    const collectionInstance = collection(this.firestore, 'users');
+    addDoc(collectionInstance, f.value).then(() => {
+      console.log('Data Saved Successfully DSS');
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
 import { environment } from '../environments/environment';
 import { provideFirestore,getFirestore } from '@angular/fire/firestore';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -14,6 +15,7 @@ import { provideFirestore,getFirestore } from '@angular/fire/firestore';
   imports: [
     BrowserModule,
     AppRoutingModule,
+    FormsModule,
     provideFirebaseApp(() => initializeApp(environment.firebase)),
     provideFirestore(() => getFirestore())
   ],
